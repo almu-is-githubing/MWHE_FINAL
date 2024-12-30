34d0a3744c09a0655e43988c1218a880d6a4bdd0
Eva Gallego <evagallegoortega@gmail.com>
2024-12-30 19:43:44 +0100

    Añadido modal form

--- js/Homepage.js
+++ js/Homepage.js
@@ -1,1 +1,17 @@
 console.log("hello world");
+
+$(document).ready(function () {
+    const $form = $(".formulario__incidencias");
+    const $submitButton = $("#submit");
+  
+    $form.on("input", function () {
+      // Verifica si todos los campos son válidos
+      const allFieldsFilled = $form
+        .find("input, textarea")
+        .toArray()
+        .every((input) => input.checkValidity());
+  
+      // Habilita o deshabilita el botón según el estado de los campos
+      $submitButton.prop("disabled", !allFieldsFilled);
+    });
+  });
