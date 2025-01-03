70680b4d63cda6357990db427223270d6859ecc9
Ayme027 <aymejuliangut@gmail.com>
2025-01-03 21:58:03 +0100

    Footer_terminado

--- js/Homepage.js
+++ js/Homepage.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+const acordeons = document.querySelectorAll(".footer_acordeonDisplay");
+
+acordeons.forEach((footer_acordeonDisplay) => {
+    footer_acordeonDisplay.addEventListener("click", () => {
+        footer_acordeonDisplay.classList.toggle("active")
+    });
+});
